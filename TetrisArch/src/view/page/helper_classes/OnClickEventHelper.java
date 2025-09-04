df32dc047e9f2a5525070ee941ee96579e6c23b3
Amsdek <aessd233@163.com>
2025-09-04 18:23:59 +0800

    MenuView: Implement basic menu framework
    - init: implement window initialization
    - draw: implement rendering logic
    - handleInput: implement button click handling

---
+++ TetrisArch/src/view/page/helper_classes/OnClickEventHelper.java
@@ -0,0 +1,29 @@
+package view.page.helper_classes;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+public class OnClickEventHelper {
+
+    public static void setOnClickColor(JButton button, Color pressedColor, Color originalColor) {
+    button.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                button.setContentAreaFilled(false);
+                button.setBackground(pressedColor);
+                button.setOpaque(true);
+                button.repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button.setBackground(originalColor);
+                button.repaint();
+            }
+        });
+    }
+
+}
