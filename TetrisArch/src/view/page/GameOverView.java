ec10cf0408cfd71585176ef7a3f21b5238f375a3
nhz <nhz3312916852@qq.com>
2025-09-07 15:49:31 +0800

    GameOverView:解决了失败界面不居中问题

--- TetrisArch/src/view/page/GameOverView.java
+++ TetrisArch/src/view/page/GameOverView.java
@@ -24,12 +24,8 @@ public class GameOverView extends AbstractGameView {
 
     //初始化
     public GameOverView() {
-        //设置界面大小
-        this.setSize(Global.wide, Global.height);
         //设置界面关闭模式
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //设置界面居中
-        this.setLocationRelativeTo(null);
         //设置界面可视化
         this.setVisible(false);
         //标题
@@ -87,6 +83,8 @@ public class GameOverView extends AbstractGameView {
 
         this.add(gameOverLabel);
         this.pack();
+        //设置界面居中
+        this.setLocationRelativeTo(null);
 
     }
 
