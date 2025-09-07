1b9af73e2bc23a0666b0fc6f82b91e804fa9aa9b
krj <suxiao0707@gmail.com>
2025-09-07 17:03:49 +0800

    Scoring:修改得分规则

--- TetrisArch/src/controller/Scoring.java
+++ TetrisArch/src/controller/Scoring.java
@@ -7,16 +7,13 @@ public final class Scoring {
 	//计算分数，几行得几分
 	public static int scoreForClears(int lines) {
 
-		if (lines <= 0) {
-			return 0;
-		}
-		switch(lines){
-			case 1: return 1;
-			case 2: return 3;
-			case 3: return 5;
-			case 4: return 8;
-			default: return lines*2+1;
-		}
+		return switch (lines) {
+            case 1 -> 1;
+            case 2 -> 3;
+            case 3 -> 5;
+            case 4 -> 8;
+            default -> 0;
+        };
 	}
 }
 
